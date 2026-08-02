3623547e483832fd800b7fbb5b317042e909b991
luna460128307 <49060551+luna460128307@users.noreply.github.com>
2026-08-02 23:14:29 +0800

    Update index.js

--- server/index.js
+++ server/index.js
@@ -111,7 +111,7 @@ app.get('*', (req, res) => {
 });
 
 const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => {
+app.listen(PORT, '0.0.0.0', () => {
   console.log(`Atoms Clone Demo server running at http://localhost:${PORT}`);
 });
 
